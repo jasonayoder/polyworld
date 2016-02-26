3cb9f2444ca4e8a6c919635ca84cb96bc633aeae
Jason Yoder <jasonayoder@gmail.com>
2016-02-26 16:15:36 -0500

    adding recompile for br2

---
+++ recompile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+
+module load python/2.7.8
+module load gsl/1.15
+module load qt/gnu/4.8.6
+
+module unload opengl
+
+make
+
+cd src/app
+cat .qt.mak_patch >.qt.mak
+make -f .qt.mak
+cd ../..
+
+make
+
+cd src/tools/PwMoviePlayer
+cat .qt.mak_patch >.qt.mak
+make -f .qt.mak
+cd ../../..
+
+make
+
+touch bin/proputil
+make
+
+touch bin/pmvutil
+make
+
+
+module load opengl
+
+
+module unload python/2.7.8
+module unload gsl/1.15
+module unload qt/gnu/4.8.6
